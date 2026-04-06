6250ec708cfeaba3501291dadc85e508bab46c7f
schundi1405 <schundi1405@gmail.com>
2026-04-06 17:53:40 -0400

    Initial Commit

---
+++ trivvey(take2)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trivvey(take2)
+//
+//  Created by Sindhu's iPad on 4/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trivvey(take2)/trivvey_take2_App.swift
@@ -0,0 +1,17 @@
+//
+//  trivvey_take2_App.swift
+//  trivvey(take2)
+//
+//  Created by Sindhu's iPad on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct trivvey_take2_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
